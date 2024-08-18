03a4856d47a065b7c6bc57492565f051b4a114b3
jacxd64 <70988180+jacxd64@users.noreply.github.com>
2024-08-18 16:52:05 -0500

    Initial Commit

---
+++ Calculator WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Calculator_WebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
